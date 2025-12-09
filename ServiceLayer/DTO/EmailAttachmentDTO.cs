8e2efcd380313c43d7f6b1ca2664d9e9f793f74b
Disa-Sazhu <disazhinidisa@gmail.com>
2025-12-09 21:01:40 +0530

    mail2

---
+++ ServiceLayer/DTO/EmailAttachmentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.DTO
+{
+    public class EmailAttachmentDto
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
